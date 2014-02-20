db83f838113991b6db429c39a2730e629756151e
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-20 11:58:26 -0600

    html loops

--- public/test.php
+++ public/test.php
@@ -1,1 +1,21 @@
-<?php echo "hello"; ?>
+<html>
+	<head>
+		<title>Our Shop</title>
+	</head>
+	<body>
+		<p>	
+			<?php
+				$items = 6;
+
+			?>
+
+		</p>	
+<script type="text/javascript">
+		bands.forEach(function(element, index, array){
+		//do
+	});
+</script>		
+	</body>
+
+
+</html>
