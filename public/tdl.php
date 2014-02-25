ff4a0f509534e2b2d7b15ac79a873aafb0bbb1c9
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-25 14:07:14 -0600

    tdl

--- public/tdl.php
+++ public/tdl.php
@@ -1,21 +1,23 @@
-<!DOCTYPE html>
 
 <?php
 
-$items = array();
+$basename = "/vagrant/sites/codeup.dev/public";
 
-function addfile($alpha, &$array) {
+function addfile($alpha) {
 
     $handle = fopen($alpha, "r");
     $contents = fread($handle, filesize($alpha));
     $contents_array = explode("\n", $contents);
-    $result = array_merge($array, $contents_array);
-    $array = $result;
+    // $result = array_merge($array, $contents_array);
+    // $array = $result;
     fclose($handle);
-    return $array;
+    return $contents_array;
 
 
     }
+
+    // $empty = array();
+    $items = addfile("todo.txt");
 
 function get_input($input) {
       
@@ -36,27 +38,54 @@ function save($array, $file) {
     }
 
 }
+         
 
 
-       if (isset($_POST['newitem'])) {
+       if (isset($_POST['newitem']) && !empty($_POST['newitem'])) {
             $item = $_POST['newitem'];
 
 
             array_push($items, $item);
+
+             save($items, "todo.txt");
+             unset($_POST);
        }
 
-        //$fn = $_POST;
-
-         $things = addfile("todo.txt", $items);
 
          if (isset($_GET['remove'])) {
 
             $itemId = $_GET['remove'];
             unset($items[$itemId]);
-
-             save($items, "todo.txt");
+            unset($_GET);
+            // unset($_GET);
+            //  header("Location: tdl.php");
+            // exit(0);
+            save($items, "todo.txt");
+            var_dump($items);
          }
+
+         if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+            // Set the destination directory for uploads
+            //$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+            // Grab the filename from the uploaded file by using basename
+            $filename = basename($_FILES['file1']['name']);
+            // Create the saved filename using the file's original name and our upload directory
+            $saved_filename = $basename . $filename;
+            // Move the file from the temp location to our uploads directory
+            move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+            //var_dump($_FILES);
+
+          // Check if we saved a file
+        }
+          if (isset($saved_filename)) {
+
+              // If we did, show a link to the uploaded file
+              echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+          }
 ?>
+
+<!DOCTYPE html>
 
 <html>
 <head>
@@ -72,17 +101,32 @@ function save($array, $file) {
                 }
 
 
-
+            
+             //var_dump($items);
+             
+            
 
 
         ?>
     </ul>
 
-<h2>Add file</h2>
+<h2>Add Item</h2>
 <form method="POST" action="">
     
         <input id="newitem" name="newitem" type="text">
         <input type="submit" value="add">
+</form>
+
+<h1>Upload File</h1>
+
+<form method="POST" enctype="multipart/form-data">
+    <p>
+        <label for="file1">File to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
+    <p>
+        <input type="submit" value="Upload">
+    </p>
 </form>
 </body>
 </html>
---
+++ public/tdl2.php
@@ -0,0 +1,97 @@
+<?php 
+     
+ function save_to_file($filename, $items) {
+      $string = implode("\n", $items);
+    $handle = fopen($filename, 'w');
+    fwrite($handle, $string);
+    fclose($handle);
+    }
+
+  function loadFile($filename){
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    fclose($handle);
+    return explode("\n", $contents);
+
+  }
+
+    $filename = 'todo.txt';
+
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    $items = explode("\n", $contents);
+    fclose($handle);
+    
+ 
+  if (!empty($_POST["newitem"])){
+    $item = $_POST["newitem"];
+    array_push($items, $item);
+    save_to_file($filename, $items);
+  }
+  //remove
+  if (isset($_GET['remove'])){
+    unset($items[$_GET['remove']]);
+    save_to_file($filename, $items);
+    header("location: tdl2.php");
+    exit;
+  }
+
+  // Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0) {
+    // Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/codeup.dev/public';
+    // Grab the filename from the uploaded file by using basename
+    $filename = basename($_FILES['file1']['name']);
+    // Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $filename;
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+
+    $new_items = loadFile($saved_filename);
+    $items = array_merge($items, $new_items);
+
+}
+
+// Check if we saved a file
+if (isset($saved_filename)) {
+
+    var_dump($saved_filename);
+    // If we did, show a link to the uploaded file
+    echo "<p>You can download your file <a href='/{$filename}'>here</a>.</p>";
+}
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>TODO List</title>
+</head>
+<body>
+
+  <h2>TODO List</h2>
+  <ul>
+    <?php foreach ($items as $key => $item) {
+      $newTodo = $key + 1;
+      echo "<li>$item <a href='?remove=$key'>Remove Item</a></li>";
+} 
+?>
+  </ul>
+    <form method="POST">
+      <input type="text" id="newitem" name="newitem" autofocus="autofocus" placeholder="add item">
+      <input type="submit" value="add" >
+    </form>
+    <h1>Upload File</h1>
+
+<form method="POST" enctype="multipart/form-data">
+    <p>
+        <label for="file1">File to upload: </label>
+        <input type="file" id="file1" name="file1">
+    </p>
+    <p>
+        <input type="submit" value="Upload">
+    </p>
+</form>
+</body>
+</html>
