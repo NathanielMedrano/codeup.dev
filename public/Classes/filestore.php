1c4f990d42f313d0792620773d898af150bad386
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-28 11:29:08 -0600

    Address Book with Class and Object

---
+++ public/Classes/filestore.php
@@ -0,0 +1,53 @@
+<?php
+
+class Filestore {
+
+    public $filename = '';
+
+    function __construct($filename = 'address_book.csv') 
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * Returns array of lines in $this->filename
+     */
+   function read_file() {
+        $contents = [];
+        $handle = fopen($this->filename, "r");
+        while (($data = fgetcsv($handle)) !== FALSE) {
+            $contents[] = $data;
+        }
+        fclose($handle);
+        return $contents;
+    }
+
+    /**
+     * Writes each element in $array to a new line in $this->filename
+     */
+    function save_file($address_book){
+            $handle = fopen($this->filename, 'w');
+            foreach ($address_book as $row) {
+                fputcsv($handle, $row);
+                }
+            fclose($handle);
+        }
+
+
+    /**
+     * Reads contents of csv $this->filename, returns an array
+     */
+    function read_csv()
+    {
+
+    }
+
+    /**
+     * Writes contents of $array to csv $this->filename
+     */
+    function write_csv($array)
+    {
+
+    }
+
+}
--- public/Classes/todo_class.php
+++ public/Classes/todo_class.php
@@ -1,31 +1,42 @@
 <?php
 
-class AddressData {
+// class AddressData {
 
-	public $filename = '';
+// 	public $filename = '';
 
-	function __construct($filename = 'address_book.csv'){
-		$this->filename = $filename;
-	}
+// 	function __construct($filename = 'address_book.csv'){
+// 		$this->filename = $filename;
+// 	}
 
-	function read_file() {
-		$contents = [];
-		$handle = fopen($this->filename, "r");
-		while (($data = fgetcsv($handle)) !== FALSE) {
-			$contents[] = $data;
-		}
-		fclose($handle);
-		return $contents;
-	}
+// 	function read_file() {
+// 		$contents = [];
+// 		$handle = fopen($this->filename, "r");
+// 		while (($data = fgetcsv($handle)) !== FALSE) {
+// 			$contents[] = $data;
+// 		}
+// 		fclose($handle);
+// 		return $contents;
+// 	}
 
-	function save_file($address_book){
-			$handle = fopen($this->filename, 'w');
-			foreach ($address_book as $row) {
-				fputcsv($handle, $row);
-				}
-			fclose($handle);
-		}
+// 	function save_file($address_book){
+// 			$handle = fopen($this->filename, 'w');
+// 			foreach ($address_book as $row) {
+// 				fputcsv($handle, $row);
+// 				}
+// 			fclose($handle);
+// 		}
 
+
+// }
+
+
+require_once('filestore.php');
+
+class AddressData extends Filestore {
+
+function book() {
+	echo "test";
+}
 
 }
 
