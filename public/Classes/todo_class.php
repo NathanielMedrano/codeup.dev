af5dea6c4d5d773b066a1532d4678359b46a72d0
NathanielMedrano <ndmedrano@yahoo.com>
2014-02-28 09:48:31 -0600

    Address With Classes
    Improved with include that attaches class

---
+++ public/Classes/todo_class.php
@@ -0,0 +1,68 @@
+<?php
+
+class AddressData {
+
+	public $filename = '';
+
+	function __construct($filename = 'address_book.csv'){
+		$this->filename = $filename;
+	}
+
+	function read_file() {
+		$contents = [];
+		$handle = fopen($this->filename, "r");
+		while (($data = fgetcsv($handle)) !== FALSE) {
+			$contents[] = $data;
+		}
+		fclose($handle);
+		return $contents;
+	}
+
+	function save_file($address_book){
+			$handle = fopen($this->filename, 'w');
+			foreach ($address_book as $row) {
+				fputcsv($handle, $row);
+				}
+			fclose($handle);
+		}
+
+
+}
+
+$book = new AddressData();
+
+$address_book = $book->read_file();
+
+
+$book->save_file($address_book);
+
+$errorMessage =[];
+
+if (!empty($_POST)){
+	$entry =[];
+	$entry['name'] = $_POST['name'];
+	$entry['address'] = $_POST['address'];
+	$entry['city'] = $_POST['city'];
+	$entry['state'] = $_POST['state'];
+	$entry['zip'] = $_POST['zip'];
+
+	foreach ($entry as $key => $value){
+		if (empty($value)) {
+			array_push($errorMessage, "$key must have value.");
+		}
+	}
+	if(empty($errorMessage)) {
+	array_push($address_book, array_values($entry));
+	$book->save_file($address_book);
+	}
+}
+
+if (isset($_GET['remove'])){
+	unset($address_book[$_GET['remove']]);
+	$book->save_file($address_book);
+
+}
+
+
+
+?>
--- public/address_book.php
+++ public/address_book.php
@@ -1,116 +1,87 @@
 <?php
 
-class AddressDataStore {
+include('classes/todo_class.php');
 
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0){
+	if ($_FILES['file1']['type'] != 'text/csv') {
+		$errorMsg = 'Invalid File type';
+		echo $errorMsg;
+	} else {
+		$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+		$new_file = basename($_FILES['file1']['name']);
+		$saved_filename = $upload_dir . $new_file;
+		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
 
-	function __construct($filename = 'address_book1.csv'){
-		$this->filename = $filename;
-	}
+	    $newfile = new AddressData($saved_filename);
 
-    public $filename = '';
+	    $addFile = $newfile->read_file();
 
-
-function csv($items) {
-	$handle = fopen('add_book1.csv', 'a+');
-
-	foreach ($items as $fields) {
-
-		$fields = explode(' ', $fields);
-
-	    fputcsv($handle, $fields);
-	}
-
-	fclose($handle);
-
+	    if (isset($_POST['over1']) && $_POST['over1'] == TRUE){
+	    	$address_book = $addFile;
+	    }else{
+			foreach ($addFile as $key => $item) {
+	        	array_push($address_book, $addFile[$key]);
+	    	}
+		} 
+    $book->save_file($address_book);    
+    }
 }
-
-function loadFile($filename){
-    $handle = fopen($filename, 'r');
-    $contents = fread($handle, filesize($filename));
-    fclose($handle);
-    return explode("\n", $contents);
-
-  }
-}
-
-$items = array();
-
-$errors = array();
-
-if (!empty($_POST["name"])){
-
-    $item = htmlspecialchars(strip_tags($_POST["name"]));
-    array_push($items, $item);
-    //save_to_file($filename, $items);
-  } 
-
-if (!empty($_POST["address"])){
-	
-    $item = htmlspecialchars(strip_tags($_POST["address"]));
-    array_push($items, $item);
-    //save_to_file($filename, $items);
-  } 
-
-if (!empty($_POST["city"])){
-	
-    $item = htmlspecialchars(strip_tags($_POST["city"]));
-    array_push($items, $item);
-    //save_to_file($filename, $items);
-  } 
-
-if (!empty($_POST["zip"])){
-	
-    $item = htmlspecialchars(strip_tags($_POST["zip"]));
-    array_push($items, $item);
-    //save_to_file($filename, $items);
-  } 
-
-  if (!empty($_POST["phone"])){
-	
-    $item = htmlspecialchars(strip_tags($_POST["phone"]));
-    array_push($items, $item);
-    //save_to_file($filename, $items);
-  } 
-
-  
-
-	csv($items);
-
-	$test = loadFile('add_book1.csv');
 
 ?>
 
+<!DOCTYPE html>
+
 <html>
 <head>
-	<title>Address Book</title>
+	<title>Addressbook</title>
 </head>
 <body>
 
-<h2>Address Info:</h2>
-  <ul>
+	<h2>Address Book</h2>
 
+	<table>
+			<? foreach ($address_book as $key => $row) { ?> 
+				<tr>
+				<? foreach ($row as $entry) { ?>
+					 <?= "<td>" . htmlspecialchars(strip_tags($entry))  .  "</td>"; } ?>
+					<td> <a href='?remove=<?=$key ?>'>Remove Item</a></td>
+				<? } ?>
 
-  </ul>
-
-<form action="address_book.php" method="post">
-Name: <input type="text" name="name" > <br>
-Address: <input type="text" name="address"><br>
-City: <input type="text" name="city"><br>
-Zip: <input type="text" name="zip"><br>
-Phone: <input type="text" name="phone"><br>
-<input type="submit">
-</form>
-
-<table>
-	<tr>
-		<? foreach ($test as $fields => $value) :  ?>
-	    <td><?= $value;?></td>
-		<? endforeach; ?>
-	</tr>	
-		
+				</tr>
+	</table>
 	
-
-</table>
+	<form method="POST" enctype="multipart/form-data" action="/address_book.php">
+		<p>
+			<label>Name: </label>
+			<input type="text" name="name" id="name" >
+		</p>
+		<p>
+			<label>Address: </label>
+			<input type="text" name="address" id="address" >
+		</p>
+		<p>
+			<label>City: </label>
+			<input type="text" name="city" id="city" >
+		</p>
+		<p>
+			<label>State: </label>
+			<input type="text" name="state" id="state" >
+		</p>
+		<p>
+			<label>Zip: </label>
+			<input type="text" name="zip" id="zip" >
+		</p>
+		<p>
+			<input type="submit" value="add" >
+		</p>
+		<p>
+			<label for="file1">Add File:</label>
+			<input type="file" id="file1" name="file1" >
+		</p>
+		<P>
+			<input type="submit" value="Upload">
+			<label><input type="checkbox" id="over1" name="over1" value="checked">Over Write</label>
+		</P>
 
 </body>
 </html>
