1f434510ccaa9503f1c73c4ee01531e6a693485b
NathanielMedrano <ndmedrano@yahoo.com>
2014-03-13 14:12:09 -0500

    PHP with MySQL
    Established error messages for mysql connection

---
+++ public/php_mysqli.php
@@ -0,0 +1,11 @@
+<?php
+
+// Get new instance of MySQLi object
+$mysqli = @new mysqli('127.0.0.1', 'codeup', 'password', 'employees');
+
+// Check for errors
+if ($mysqli->connect_errno) {
+    echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
