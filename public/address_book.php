2748c7a1e1775715b2324293ef07962f0b6b6c9b
NathanielMedrano <ndmedrano@yahoo.com>
2014-03-19 11:17:22 -0500

    Address Book with mysql
    made connection to input data

--- public/address_book.php
+++ public/address_book.php
@@ -1,92 +1,111 @@
 <?php
 
-require_once('classes/todo_class.php');
-
-$filename = 'address_book.csv';
-
-if (count($_FILES) > 0 && $_FILES['file1']['error'] == 0){
-	if ($_FILES['file1']['type'] != 'text/csv') {
-		$errorMsg = 'Invalid File type';
-		echo $errorMsg;
-	} else {
-		$upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
-		$new_file = basename($_FILES['file1']['name']);
-		$saved_filename = $upload_dir . $new_file;
-		move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-
-	    $newfile = new AddressData($saved_filename);
-
-	    $addFile = $newfile->read_file();
-
-	    if (isset($_POST['over1']) && $_POST['over1'] == TRUE){
-	    	$address_book = $addFile;
-	    }else{
-			foreach ($addFile as $key => $item) {
-	        	array_push($address_book, $addFile[$key]);
-	    	}
-		} 
-    $book->save_file($address_book);    
-    }
+$errorMessage = null;
+$successMessage = null;
+ 
+// connect to the db
+$mysqli = new mysqli('127.0.0.1', 'codeup', 'password', 'codeup_mysqli_test_db');
+ 
+// Check for errors
+if ($mysqli->connect_errno)
+{
+    throw new Exception('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
+ 
+if (!empty($_POST))
+{
+  // check for new address
+  if (isset($_POST['address']))
+  {
+    if ($_POST['address'] != "")
+    {
+      $address = substr($_POST['address'], 0, 200);
+ 
+      // add to db
+      $stmt = $mysqli->prepare("INSERT INTO addresses (item) VALUES (?);");
+      $stmt->bind_param("s", $address);
+      $stmt->execute();
+ 
+      $successMessage = "Address was added successfully.";
+    }
+    else
+    {
+      // show error message
+      $errorMessage = "Please input a address.";
+    }
+  }
+  else if (!empty($_POST['remove']))
+  {
+    // remove item from db
+    $stmt = $mysqli->prepare("DELETE FROM addresses WHERE id = ?;");
+    $stmt->bind_param("i", $_POST['remove']);
+    $stmt->execute();
+ 
+    $successMessage = "Address was removed successfully.";
+  }
+}
+ 
+$todos = $mysqli->query("SELECT * FROM addresses");
+ 
+
+if (!empty($_POST["name"])){
+
+    $item = htmlspecialchars(strip_tags($_POST["name"]));
+    array_push($items, $item);
+    //save_to_file($filename, $items);
+  } 
+
+if (!empty($_POST["address"])){
+
+    $item = htmlspecialchars(strip_tags($_POST["address"]));
+    array_push($items, $item);
+    //save_to_file($filename, $items);
+  } 
+
+if (!empty($_POST["city"])){
+
+    $item = htmlspecialchars(strip_tags($_POST["city"]));
+    array_push($items, $item);
+    //save_to_file($filename, $items);
+  } 
+
+if (!empty($_POST["zip"])){
+
+    $item = htmlspecialchars(strip_tags($_POST["zip"]));
+    array_push($items, $item);
+    //save_to_file($filename, $items);
+  } 
+
+  if (!empty($_POST["phone"])){
+
+    $item = htmlspecialchars(strip_tags($_POST["phone"]));
+    array_push($items, $item);
+    //save_to_file($filename, $items);
+  } 
+
+  
 
 ?>
 
-<!DOCTYPE html>
-
 <html>
 <head>
-	<title>Address book</title>
+  <title>Address Book</title>
 </head>
 <body>
 
-	<h2>Address Book</h2>
+<h2>Address Info:</h2>
+  <ul>
 
-	<table>
-			<? foreach ($address_book as $key => $row) { ?> 
-				<tr>
-					<?  if (!is_string($row)) {
-                throw new Exception('Must be a string');
-            } ?>
-				<? foreach ($row as $entry => $value) { ?>
-					 <?= "<td>" . htmlspecialchars(strip_tags($value))  .  "</td>"; } ?>
-					<td> <a href='?remove=<?=$key ?>'>Remove Item</a></td>
-				<? } ?>
 
-				</tr>
-	</table>
-	
-	<form method="POST" enctype="multipart/form-data" action="/address_book.php">
-		<p>
-			<label>Name: </label>
-			<input type="text" name="name" id="name" >
-		</p>
-		<p>
-			<label>Address: </label>
-			<input type="text" name="address" id="address" >
-		</p>
-		<p>
-			<label>City: </label>
-			<input type="text" name="city" id="city" >
-		</p>
-		<p>
-			<label>State: </label>
-			<input type="text" name="state" id="state" >
-		</p>
-		<p>
-			<label>Zip: </label>
-			<input type="text" name="zip" id="zip" >
-		</p>
-		<p>
-			<input type="submit" value="add" >
-		</p>
-		<p>
-			<label for="file1">Add File:</label>
-			<input type="file" id="file1" name="file1" >
-		</p>
-		<P>
-			<input type="submit" value="Upload">
-			<label><input type="checkbox" id="over1" name="over1" value="checked">Over Write</label>
-		</P>
+  </ul>
+
+<form action="address_book.php" method="post">
+Name: <input type="text" name="name" > <br>
+Address: <input type="text" name="address"><br>
+City: <input type="text" name="city"><br>
+Zip: <input type="text" name="zip"><br>
+Phone: <input type="text" name="phone"><br>
+<input type="submit">
 
 </body>
 </html>
--- public/todo-db.php
+++ public/todo-db.php
@@ -4,32 +4,51 @@ var_dump($_GET);
  
 var_dump($_POST);
  
+$errorMessage = null;
+$successMessage = null;
+ 
 // connect to the db
 $mysqli = new mysqli('127.0.0.1', 'codeup', 'password', 'codeup_mysqli_test_db');
  
 // Check for errors
-if ($mysqli->connect_errno) {
+if ($mysqli->connect_errno)
+{
     throw new Exception('Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
-
+ 
 if (!empty($_POST))
 {
-
-	if ($_POST['todo']))
+	// check for new todo
+	if (isset($_POST['todo']))
 	{
 		if ($_POST['todo'] != "")
 		{
-			//add to db
-		}	else
+			$todo = substr($_POST['todo'], 0, 200);
+ 
+			// add to db
+			$stmt = $mysqli->prepare("INSERT INTO todos (item) VALUES (?);");
+			$stmt->bind_param("s", $todo);
+			$stmt->execute();
+ 
+			$successMessage = "Todo item was added successfully.";
+		}
+		else
 		{
-			//show err
-		}	
+			// show error message
+			$errorMessage = "Please input a todo item.";
+		}
 	}
-	elseif (!empty($_POST['remove'])) {
-		//remove item from db
+	else if (!empty($_POST['remove']))
+	{
+		// remove item from db
+		$stmt = $mysqli->prepare("DELETE FROM todos WHERE id = ?;");
+		$stmt->bind_param("i", $_POST['remove']);
+		$stmt->execute();
+ 
+		$successMessage = "Todo item was removed successfully.";
 	}
-
-} 
+}
+ 
 $todos = $mysqli->query("SELECT * FROM todos");
  
 ?>
@@ -44,11 +63,18 @@ $todos = $mysqli->query("SELECT * FROM todos");
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
  
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<!-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script> -->
 </head>
 <body>
  
 <div class="container">
+ 
+	<? if (!empty($successMessage)): ?>
+		<div class="alert alert-success"><?= $successMessage; ?></div>
+	<? endif; ?>
+	<? if (!empty($errorMessage)): ?>
+		<div class="alert alert-danger"><?= $errorMessage; ?></div>
+	<? endif; ?>
  
 	<h1>Todo List</h1>
  
@@ -64,8 +90,8 @@ $todos = $mysqli->query("SELECT * FROM todos");
 	<h2>Add Items</h2>
 	<form class="form-inline" role="form" action="todo-db.php" method="POST">
 		<div class="form-group">
-			<label class="sr-only" for="exampleInputEmail2">Todo Item</label>
-			<input type="text" name="todo" class="form-control" placeholder="Enter todo item">
+			<label class="sr-only" for="todo">Todo Item</label>
+			<input type="text" name="todo" id="todo" class="form-control" placeholder="Enter todo item">
 		</div>
 		<button type="submit" class="btn btn-default">Add Todo</button>
 	</form>
